6ab13b6784b23ac4de5f822186f8fa0060df9d6e
ChristianSalon <80092102+ChristianSalon@users.noreply.github.com>
2022-07-28 22:26:12 +0200

    Home page

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#063183",
+        primaryLight: "#E6F0FF",
+        primaryDark: "#0F1B4C",
+      },
+    },
+  },
+  plugins: [],
+};
